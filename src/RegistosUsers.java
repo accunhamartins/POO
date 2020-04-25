d856a9c9f57a458b44f2bc5e2ff9d401dc0af77d
141Hash <accmartins3000@gmail.com>
2020-04-25 23:19:03 +0100

    Registos

--- src/RegistosUsers.java
+++ src/RegistosUsers.java
@@ -1,13 +1,51 @@
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class RegistosUsers {
 
-    private List<Utilizador> users;
+    private Map<String, Utilizador> users;
 
     private RegistosUsers(){
-        this.users = new ArrayList<>();
+        this.users = new TreeMap<>();
     }
 
+    private RegistosUsers(Map<String, Utilizador> user){
+        setUsers(user);
+    }
+
+    private RegistosUsers(RegistosUsers r){
+        setUsers(r.getUsers());
+    }
+
+    public Map<String, Utilizador> getUsers() {
+        return this.users.entrySet().stream().collect(Collectors.toMap(r -> r.getKey(), r -> r.getValue().clone()));
+    }
+
+    public void setUsers(Map<String, Utilizador> users) {
+        this.users = new TreeMap<>();
+        users.entrySet().forEach(e -> this.users.put(e.getKey(), e.getValue().clone()));
+    }
+
+
+    public RegistosUsers clone(){
+        return (new RegistosUsers(this));
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("Total de utilizadores: ").append("\n");
+        sb.append(this.users);
+
+        return sb.toString();
+    }
+
+    public boolean equals(Object obj){
+        if(obj == this) return true;
+        if(obj == null || obj.getClass() != this.getClass()) return false;
+        RegistosUsers r = (RegistosUsers) obj;
+        return this.users.equals((r.getUsers()));
+    }
 
 }
